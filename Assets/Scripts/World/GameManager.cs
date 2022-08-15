5a0904a784d74637b3b05abda9c2960427929a2b
Joe Fabel <joseph.fabel@gmail.com>
2022-08-14 18:05:39 -0700

    Updated TODOs

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -51,7 +51,6 @@ public class GameManager : MonoBehaviour
     {
         Debug.LogWarning("TODO - Add some NPC quests");
         Debug.LogWarning("TODO - Add a  shop");
-        Debug.LogWarning("TODO - Enable skeleton character only after the quest has been completed (json or extra setting file controlling available characters)");
         Debug.LogWarning("TODO - Add 1x buy of water in a store");
         Debug.LogWarning("TODO - Add quick item HUD");
         Debug.LogWarning("TODO - Let projectiles destroy bombs early");
